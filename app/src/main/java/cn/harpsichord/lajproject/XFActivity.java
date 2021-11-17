a1e1ddd90f268fd2e582c994aed1e697f171c690
Derek <liutao3@cig.com.cn>
2021-11-17 23:18:17 +0800

    save

--- app/src/main/java/cn/harpsichord/lajproject/XFActivity.java
+++ app/src/main/java/cn/harpsichord/lajproject/XFActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -24,12 +26,6 @@ import com.iflytek.cloud.SynthesizerListener;
 public class XFActivity extends AppCompatActivity {
 
     private static final String TAG = "XFActivity";
-
-    private InitListener initListener = i -> {
-        if (i != ErrorCode.SUCCESS) {
-            Toast.makeText(XFActivity.this, "Failed to init listener!", Toast.LENGTH_SHORT).show();
-        }
-    };
 
     private RecognizerListener recognizerListener = new RecognizerListener() {
         @Override
@@ -63,6 +59,46 @@ public class XFActivity extends AppCompatActivity {
         }
     };
 
+    private SynthesizerListener synthesizerListener = new SynthesizerListener() {
+        @Override
+        public void onSpeakBegin() {
+
+        }
+
+        @Override
+        public void onBufferProgress(int i, int i1, int i2, String s) {
+
+        }
+
+        @Override
+        public void onSpeakPaused() {
+
+        }
+
+        @Override
+        public void onSpeakResumed() {
+
+        }
+
+        @Override
+        public void onSpeakProgress(int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onCompleted(SpeechError speechError) {
+            if (speechError != null) {
+                Toast.makeText(XFActivity.this, "发生错误，请重试！", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, speechError.toString());
+            }
+        }
+
+        @Override
+        public void onEvent(int i, int i1, int i2, Bundle bundle) {
+
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,47 +109,28 @@ public class XFActivity extends AppCompatActivity {
         }
         SpeechUtility.createUtility(this, SpeechConstant.APPID +"=cc41d66f");
 
+        // speak
         SpeechSynthesizer synthesizer = SpeechSynthesizer.createSynthesizer(this, null);
         synthesizer.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");
         synthesizer.setParameter(SpeechConstant.SPEED, "50");
         synthesizer.setParameter(SpeechConstant.VOLUME, "100");
         synthesizer.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
-        synthesizer.startSpeaking("你好", new SynthesizerListener() {
-            @Override
-            public void onSpeakBegin() {
 
-            }
+        Button speak = findViewById(R.id.speak_button);
+        EditText content = findViewById(R.id.iat_text);
 
-            @Override
-            public void onBufferProgress(int i, int i1, int i2, String s) {
-
-            }
-
-            @Override
-            public void onSpeakPaused() {
-
-            }
-
-            @Override
-            public void onSpeakResumed() {
-
-            }
-
-            @Override
-            public void onSpeakProgress(int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onCompleted(SpeechError speechError) {
-
-            }
-
-            @Override
-            public void onEvent(int i, int i1, int i2, Bundle bundle) {
-
+        speak.setOnClickListener(v -> {
+            String s = content.getText().toString().trim();
+            if ("".equals(s)) {
+                Toast.makeText(this, "请输入文本", Toast.LENGTH_SHORT).show();
+            } else {
+                synthesizer.startSpeaking(s, synthesizerListener);
             }
         });
 
+        // recognise
+        SpeechRecognizer recognizer = SpeechRecognizer.createRecognizer(this, recognizerListener);// TODO listener
+        recognizer.setParameter(SpeechConstant.RESULT_TYPE, "plain");
+        recognizer.setParameter(SpeechConstant.VAD_EOS, "3000");
     }
 }
