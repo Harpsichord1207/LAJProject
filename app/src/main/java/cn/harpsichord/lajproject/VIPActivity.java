9cb4441c6ae06c0af5235f0cfeb9fdfb50ffb98b
Harpsichord <feelingsxxx@gmail.com>
2021-11-30 10:34:58 +0800

    get vip info

--- app/src/main/java/cn/harpsichord/lajproject/VIPActivity.java
+++ app/src/main/java/cn/harpsichord/lajproject/VIPActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,8 +14,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import okhttp3.Call;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class VIPActivity extends AppCompatActivity {
+
+    private static String url;
+    private static String secret;
+    private static String fullUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +43,16 @@ public class VIPActivity extends AppCompatActivity {
         String filePath = this.getExternalFilesDir("secret") + "/" + "secret.txt";
         File file = new File(filePath);
 
-        StringBuilder text = new StringBuilder();
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
 
             while ((line = br.readLine()) != null) {
-                text.append(line);
-                text.append('\n');
+                if (line.startsWith("url:")) {
+                    url = line.substring(4).trim();
+                } else if (line.startsWith("secret:")) {
+                    secret = line.substring(7).trim();
+                }
             }
             br.close();
         }
@@ -39,6 +61,77 @@ public class VIPActivity extends AppCompatActivity {
             Toast.makeText(this, "Failed to get secret content.", Toast.LENGTH_SHORT).show();
             finish();
         }
-        Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show();
+        long ts = System.currentTimeMillis();
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update((ts + "" + secret).getBytes());
+            String token = new BigInteger(1, md5.digest()).toString(16);
+            fullUrl = url + "?ts=" + ts + "&token=" + token;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to get MD5 instance.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        TextView userCountTextView = findViewById(R.id.user_count);
+        TextView vipCountTextView = findViewById(R.id.vip_count);
+        TextView lastVipTextView = findViewById(R.id.last_vip);
+
+        Button refreshBtn = findViewById(R.id.refresh_vip);
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
+                        Request request = new Request.Builder().url(fullUrl).get().build();
+                        Call call = client.newCall(request);
+                        try {
+                            Response response = call.execute();
+                            ResponseBody body = response.body();
+                            if (body != null) {
+                                String string = body.string();
+                                String applyTs = getKeyFromJson(string, "apply_ts");
+                                String vipCount = "VIP用户数: " + getKeyFromJson(string, "vip_count");
+                                String userCount = "用户总数: " + getKeyFromJson(string, "user_count");
+                                String lastVip = "最近一次请求VIP用户: " + getKeyFromJson(string, "last_vip");
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        userCountTextView.setText(userCount);
+                                        vipCountTextView.setText(vipCount);
+                                        lastVipTextView.setText(lastVip);
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(VIPActivity.this, "Response null point.", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Toast.makeText(VIPActivity.this, "Failed to request to " + url, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+    private String getKeyFromJson(String jsonString, String key) {
+        Pattern pattern = Pattern.compile("\"?" + key + "\"?:.*?\"?(.*?)\"?[,}]");
+        Matcher m = pattern.matcher(jsonString);
+        if (m.find()) {
+            String res = m.group(1);
+            if (res == null) {
+                return null;
+            }
+            if (res.trim().startsWith("\"")) {
+                return res.trim().substring(1);
+            }
+            return res.trim();
+        } else {
+            return null;
+        }
     }
 }
