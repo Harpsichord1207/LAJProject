a9ef9dea1808b1f1c9aa5b58621d7183233a213a
Harpsichord <feelingsxxx@gmail.com>
2021-12-01 10:11:54 +0800

    save: set vip

--- app/src/main/java/cn/harpsichord/lajproject/VIPActivity.java
+++ app/src/main/java/cn/harpsichord/lajproject/VIPActivity.java
@@ -2,12 +2,17 @@ package cn.harpsichord.lajproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -23,9 +28,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import okhttp3.Call;
+import okhttp3.MediaType;
 import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
@@ -76,46 +83,93 @@ public class VIPActivity extends AppCompatActivity {
         TextView userCountTextView = findViewById(R.id.user_count);
         TextView vipCountTextView = findViewById(R.id.vip_count);
         TextView lastVipTextView = findViewById(R.id.last_vip);
+        TextView lastVipTSTextView = findViewById(R.id.last_vip_ts);
 
         Button refreshBtn = findViewById(R.id.refresh_vip);
-        refreshBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
-                        Request request = new Request.Builder().url(fullUrl).get().build();
-                        Call call = client.newCall(request);
-                        try {
-                            Response response = call.execute();
-                            ResponseBody body = response.body();
-                            if (body != null) {
-                                String string = body.string();
-                                String applyTs = getKeyFromJson(string, "apply_ts");
-                                String vipCount = "VIP用户数: " + getKeyFromJson(string, "vip_count");
-                                String userCount = "用户总数: " + getKeyFromJson(string, "user_count");
-                                String lastVip = "最近一次请求VIP用户: " + getKeyFromJson(string, "last_vip");
+        refreshBtn.setOnClickListener(v -> new Thread(() -> {
+            OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
+            Request request = new Request.Builder().url(fullUrl).get().build();
+            Call call = client.newCall(request);
+            try {
+                Response response = call.execute();
+                ResponseBody body = response.body();
+                if (body != null) {
+                    String string = body.string();
+                    String applyTs = "最近一次请求VIP时间: " + getKeyFromJson(string, "apply_ts");
+                    String vipCount = "VIP用户数: " + getKeyFromJson(string, "vip_count");
+                    String userCount = "用户总数: " + getKeyFromJson(string, "user_count");
+                    String lastVip = "最近一次请求VIP用户: " + getKeyFromJson(string, "last_vip");
 
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        userCountTextView.setText(userCount);
-                                        vipCountTextView.setText(vipCount);
-                                        lastVipTextView.setText(lastVip);
-                                    }
-                                });
-                            } else {
-                                Toast.makeText(VIPActivity.this, "Response null point.", Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            Toast.makeText(VIPActivity.this, "Failed to request to " + url, Toast.LENGTH_SHORT).show();
+                    boolean lastIsVip = "true".equals(getKeyFromJson(string, "last_is_vip"));
+
+                    runOnUiThread(() -> {
+                        lastVipTSTextView.setText(applyTs);
+                        userCountTextView.setText(userCount);
+                        vipCountTextView.setText(vipCount);
+                        lastVipTextView.setText(lastVip);
+
+                        if (lastIsVip) {
+                            lastVipTextView.setTextColor(Color.GREEN);
+                        } else {
+                            lastVipTextView.setTextColor(Color.RED);
                         }
-                    }
-                }).start();
+
+                    });
+                } else {
+                    customToast("Response null point.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                customToast("Failed to request to " + url);
             }
-        });
+        }).start());
+
+        EditText editText = findViewById(R.id.input_eml);
+
+        Button setBtn = findViewById(R.id.set_vip);
+        setBtn.setOnClickListener(v -> new Thread(() -> {
+            OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build();
+            MediaType mediaType = MediaType.parse("application/json;charset=utf-8");
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("eml", editText.getText());
+            } catch (JSONException e) {
+                e.printStackTrace();
+                customToast("Unknown Error");
+                finish();
+            }
+            RequestBody requestBody = RequestBody.create(mediaType, String.valueOf(jsonObject));
+            Request request = new Request.Builder().url(fullUrl).post(requestBody).build();
+            Call call = client.newCall(request);
+            try {
+                Response response = call.execute();
+                if (response.code() == 404) {
+                    customToast("Invalid User!");
+                }
+
+                ResponseBody responseBody = response.body();
+
+                if (responseBody != null) {
+                    String string = responseBody.string();
+                    String code = getKeyFromJson(string, "code");
+                    if ("1".equals(code)) {
+                        customToast("User is already VIP");
+                    } else if ("2".equals(code)) {
+                        customToast("Failed to set VIP");
+                    } else if ("0".equals(code)) {
+                        customToast("Done!");
+                    } else {
+                        customToast("Unknown Response: " + string);
+                    }
+                } else {
+                    customToast("Response null point.");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                customToast("Failed to request to " + url);
+            }
+        }).start());
     }
 
     private String getKeyFromJson(String jsonString, String key) {
@@ -133,5 +187,10 @@ public class VIPActivity extends AppCompatActivity {
         } else {
             return null;
         }
+    }
+
+    private void customToast(String content) {
+        // TODO: 默认参数Toast.LENGTH_SHORT
+        runOnUiThread(() -> Toast.makeText(VIPActivity.this, content, Toast.LENGTH_SHORT).show());
     }
 }
